2a9249bf8535312a845a0a85017335eecb6fcfb4
Ryan Ford <fordryan01@hotmail.co.uk>
2025-11-17 16:36:36 +0000

    Adding a view with a httpresponse so that I can test to see if the app is connected correctly.

--- badge_manager/urls.py
+++ badge_manager/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from add_badge import views as add_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
