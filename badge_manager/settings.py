6a478a2ba92a54e6693ca29d509e433233947293
Ryan Ford <fordryan01@hotmail.co.uk>
2025-11-18 17:54:20 +0000

    Creating an env.py file to hold the secret key, updating settings file to get the new secret key from the 'env,pyu file and installing neccessary packages such as psycopg2 and dj-database-url

--- badge_manager/settings.py
+++ badge_manager/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+0qyv6_*p3sy)2%&hl--v)=axk4rp!meh1%_1o@8+ribdbv^vp'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'add_badge',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -73,13 +79,16 @@ WSGI_APPLICATION = 'badge_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -121,3 +130,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_PJTKuy87xqHM@ep-odd-bar-ag06oh93.c-2.eu-central-1.aws.neon.tech/ladle_union_crib_252952"
+)
+os.environ.setdefault("SECRET_KEY", "874649d7afcd7b33c1912aee93bd55a9")
