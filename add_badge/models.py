808a69e154c39943281cdde08dfae9d447845feb
Ryan Ford <fordryan01@hotmail.co.uk>
2025-11-20 17:20:28 +0000

    Adding the model for adding a new badge and registering it to the site within the admin file so that it can be used in the admin page

--- add_badge/models.py
+++ add_badge/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Badge(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    category = models.CharField(max_length=100, blank=True)
+    badge_image = models.ImageField()
--- badge_manager/settings.py
+++ badge_manager/settings.py
@@ -19,6 +19,8 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -135,3 +137,4 @@ CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
     "https://*.herokuapp.com"
 ]
+
